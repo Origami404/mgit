75236043cc56e0152057d9a1bc48d09fcb2d0579
Origami404 <Origami404@foxmail.com>
2021-02-09 13:09:17 +0800

    Setup argument parser

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys
+import argparse
 
+# Set Argument Parser
+args_parser = argparse.ArgumentParser(description="My Git")
+args_subparser = args_parser.add_subparsers(title="Command", dest="command")
+args_subparser.required = True
 
-if __name__ == "__main__":
-    print("Hello")
+def main(argv=sys.argv[1:]):
+    args = args_parser.parse_args()
+    
+    # now we can use args.command to get subcommons 
+
