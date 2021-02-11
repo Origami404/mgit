34e8b046bd14de44b6fa73e1c92ef98a421a4680
Origami404 <Origami404@foxmail.com>
2021-02-11 18:37:38 +0800

    Change: 格式化

--- tests/repo.py
+++ tests/repo.py
@@ -1,8 +1,10 @@
 import unittest
 import os
-from mgit.repo import create_repo, load_repo, GitRepo
+from mgit.repo import create_repo
 
 test_workpath = r'C:\Users\Administrator\Desktop\LT\mgit\tests\test_workpath'
+
+
 class TestGitRepo(unittest.TestCase):
 
     # 初始化一个测试工作目录
@@ -10,9 +12,9 @@ class TestGitRepo(unittest.TestCase):
         # 确保测试工作目录存在
         if not os.path.exists(test_workpath):
             os.makedirs(test_workpath)
-        
+
         # 清空测试工作目录
-        map(os.remove, os.listdir(test_workpath))            
+        map(os.remove, os.listdir(test_workpath))
 
     def test_create_repo(self):
         repo = create_repo(test_workpath)
@@ -20,18 +22,17 @@ class TestGitRepo(unittest.TestCase):
         def assertDirExist(*rel_path):
             path = repo.repo_dir(*rel_path)
             self.assertTrue(os.path.exists(path))
-        
-        assertDirExist('branches') 
-        assertDirExist('object') 
-        assertDirExist('refs', 'tags') 
-        assertDirExist('refs', 'heads') 
+
+        assertDirExist('branches')
+        assertDirExist('object')
+        assertDirExist('refs', 'tags')
+        assertDirExist('refs', 'heads')
 
         def assertFileContent(filename, content):
             with open(repo.repo_file(filename), 'r') as f:
                 self.assertEqual(f.read(), content)
-        
-        assertFileContent('description', \
-            "Unnamed repository; edit this file 'description' to name the repository.\n")
+
+        assertFileContent('description', "Unnamed repository; edit this file 'description' to name the repository.\n")
         assertFileContent('HEAD', 'ref: refs/heads/master\n')
 
         # TODO: 增加 Conf 的判断
