366f884bf976b83df96fee0d9e78129ddeee7c1a
Origami404 <Origami404@foxmail.com>
2021-02-11 16:40:22 +0800

    Add: Init 子命令 argparser

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import sys
 import argparse
 
 # Set Argument Parser
-args_parser = argparse.ArgumentParser(description="My Git")
-args_subparser = args_parser.add_subparsers(title="Command", dest="command")
+args_parser = argparse.ArgumentParser(prog='mgit', description='A hand-made rough but useable git implement')
+args_subparser = args_parser.add_subparsers(title='Command', dest='command')
 args_subparser.required = True
 
-def main(argv=sys.argv[1:]):
+init_parser = args_subparser.add_parser('init', help='Initialize a new, empty repository')
+init_parser.add_argument('path', default='.', help='Where to create the repository')
+
+def main():
     args = args_parser.parse_args()
-    
     # now we can use args.command to get subcommons 
 
