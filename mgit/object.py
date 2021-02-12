764fb5f5b337d039eb5c1103ae568348224515be
Origami404 <Origami404@foxmail.com>
2021-02-13 00:38:03 +0800

    Add: Tree Object

--- mgit/object.py
+++ mgit/object.py
@@ -49,6 +49,36 @@ class Commit(GitObject):
         return unparse_kvlm(self.dct, self.msg)
 
 
+class TreeItem:
+    '''储存 Tree Object 里的一个条目'''
+    def __init__(self, item_data: bytes) -> None:
+        self.deserialize(item_data)
+
+    def deserialize(self, item_data: bytes) -> None:
+        self.mode, _, item_data = item_data.partition(b' ')
+        self.name, _, self.sha1_raw = item_data.partition(b'\x00')
+
+    def serialize(self) -> bytes:
+        return self.mode + b' ' + self.name + b'\x00' + self.sha1_raw
+
+    @property
+    def sha1(self) -> str:
+        # 大端序
+        sha1_int = int.from_bytes(self.sha1_raw, 'big')
+        # 去除前导的 0x
+        return hex(sha1_int)[2:]
+
+
+class Tree(GitObject):
+    obj_type: Final[GitObjectType] = b'tree'
+
+    def deserialize(self, data: bytes) -> None:
+        self.items = list(map(TreeItem, data.split(b'\n')))
+
+    def serialize(self) -> bytes:
+        return b'\n'.join(map(lambda item: item.serialize(), self.items))
+
+
 def pack_obj(obj: GitObject) -> tuple[str, bytes]:
     data = obj.serialize()
     raw = obj.obj_type + b' ' + str(len(data)).encode('ascii') + b'\x00' + data
